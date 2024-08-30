28fb1e010371ba67b52445b516543fad3af907b3
Cesar Vega <caveligh@users.noreply.github.com>
2024-08-29 19:36:00 -0500

    Se modifica index y vite config js

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,7 @@
-import { fileURLToPath, URL } from "url";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "::",
-    port: "8080",
-  },
   plugins: [react()],
   base: '/catalina-vega-mendez.github.io/',
-  resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: fileURLToPath(new URL("./src", import.meta.url)),
-      },
-      {
-        find: "lib",
-        replacement: resolve(__dirname, "lib"),
-      },
-    ],
-  },
-});
+})
