7d66af06744f96dbb59cad48f8512552c5592688
Cesar Vega <caveligh@users.noreply.github.com>
2024-08-29 20:13:40 -0500

    Se modifica vite config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,25 @@
+import { fileURLToPath, URL } from "url";                                         
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
-
+// import path from 'path'
+import { resolve } from "path";
 export default defineConfig({
   plugins: [react()],
   base: '/catalina-vega-mendez.github.io/',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },       
+      {
+        find: "lib",
+        replacement: resolve(__dirname, "lib"),
+      },                  
+//      '@': path.resolve(__dirname, './src')
     }
   },
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      external: [/^@/]  // Esto puede ayudar con las importaciones que usan '@'
-    }
   }
 })
